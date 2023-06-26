a016f919ce38bcdf93ed5c0e3ddf264708cc8598
bindumadhavi002 <binduainampudi999@gmail.com>
2023-06-26 15:07:11 +0000

    ticket model created

--- springapp/src/main/java/com/virtusa/repository/LeadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.virtusa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LeadRepository extends JpaRepository<com.virtusa.model.Lead, Long>{
-
-}
--- springapp/src/main/java/com/virtusa/repository/SaleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.virtusa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SaleRepository extends JpaRepository<com.virtusa.model.Sale, Long>{
-
-}
