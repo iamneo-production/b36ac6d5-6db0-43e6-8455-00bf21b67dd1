feb82ea1e6e834b7ef817ed8e2b327826bc8a9cf
ANUSHA-TUMMA <anushatumma951@gmail.com>
2023-06-26 10:49:44 +0000

    files deleted

--- springapp/src/main/java/com/example/repository/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.virtusa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketRepository extends JpaRepository<com.virtusa.model.Ticket, Long>{
-
-}
