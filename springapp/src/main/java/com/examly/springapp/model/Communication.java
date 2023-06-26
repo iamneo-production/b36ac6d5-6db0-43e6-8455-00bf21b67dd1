c4892454a7103b9db34537b8c04acabdcd699fc8
ANUSHA-TUMMA <anushatumma951@gmail.com>
2023-06-26 11:47:27 +0000

    customer details

---
+++ springapp/src/main/java/com/examly/springapp/model/Communication.java
@@ -0,0 +1,10 @@
+package com.examly.springapp.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "communication")
+public class Communication {
+    // Define the properties and methods of the Communication class
+    // ...
+}
