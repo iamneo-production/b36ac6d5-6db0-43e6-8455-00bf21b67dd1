520e59a102e4659a714bc5b504bf0d1d35e04525
Alpha-Stone <sandeepmaharana569@gmail.com>
2023-07-01 11:18:29 +0000

    basic authentication (in SecurityConfig class) implemented

---
+++ springapp/src/main/java/com/examly/springapp/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,35 @@
+package main.java.com.examly.springapp.SecurityConfig;
+
+import java.beans.JavaBean;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.AbstractRequestMatcherRegistry;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+            .authorizeHttpRequests(authz->authz
+                .antMatchers("").hasRole("")
+                .antMatchers("").permitAll()  
+                .anyRequest().authenticated())
+            .formLogin(login->login
+                .loginPage("")                 // loginPage("/login")
+                .failureUrl("")                // failureUrl("/login?error")
+                .loginProcessingUrl("")
+                .defaultSuccessUrl(""))
+            .logout(logout->logout.logoutSuccessUrl("")
+                .permitAll());
+
+        return http.build();
+    }
+
+}
