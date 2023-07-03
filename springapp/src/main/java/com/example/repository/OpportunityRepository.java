79ab5116217adc3d6930687c1cba24b91aee1075
ANUSHA-TUMMA <anushatumma951@gmail.com>
2023-07-03 12:48:34 +0000

    springapp deleted

--- springapp/src/main/java/com/example/repository/OpportunityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OpportunityRepository extends JpaRepository<com.example.model.Opportunity, Long>{
-
-}
--- springapp/src/main/java/com/example/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<com.example.model.Task, Long>{
-
-}
