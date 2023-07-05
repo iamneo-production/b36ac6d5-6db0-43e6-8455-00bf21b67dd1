48f4d40d72df69a05bba36e9a4093d3ab644ba40
bindumadhavi002 <binduainampudi999@gmail.com>
2023-07-05 14:58:25 +0000

    modified navbar

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import LeadManagement from './Pages/Lead/LeadManagement';
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CustomerTable from "./Pages/Customer/CustomerTable";
+
+
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <main>
-        <LeadManagement />
-      </main>
+    // <div className="App">
+    //   <header className="App-header">
+    //   </header>
+    //   <main>
+    //   <Navbar style={{marignLeft:'50px'}}/>
+    //     <LeadManagement />
+    //   </main>
+    // </div>
+    <div>
+      <BrowserRouter>
+      <Navbar style={{marignLeft:'50px'}}/>
+      <Routes>
+        <Route path="customers">
+              <Route index element={<CustomerTable />}/>
+
+            </Route>
+        <Route path="leads">
+              <Route index element={<LeadManagement />}/>
+
+            </Route>
+      </Routes>
+      </BrowserRouter>
     </div>
+
   );
 };
 
