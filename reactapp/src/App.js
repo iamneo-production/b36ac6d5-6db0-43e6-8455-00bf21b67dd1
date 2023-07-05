c2f470a3080f28b91369676bac585ae4883ba4fa
ANUSHA-TUMMA <anushatumma951@gmail.com>
2023-07-05 13:40:13 +0000

    customer

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
-import CustomerManagement from "./components/Customer/CustomerManagement";
-import LeadManagement from "./components/Leads/LeadManagement";
+import CustomerManagement from './Pages/Customer/CustomerManagement';
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-      <Navbar style={{marignLeft:'50px'}}/>
-      <Routes>
-      <Route path="customers">
-              <Route index element={<CustomerManagement />}/>
-
-            </Route>
-      <Route path="leads">
-            <Route index element={<LeadManagement />}/>
-
-          </Route>
-      </Routes>
-      </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main>
+        <CustomerManagement />
+      </main>
     </div>
   );
 };
