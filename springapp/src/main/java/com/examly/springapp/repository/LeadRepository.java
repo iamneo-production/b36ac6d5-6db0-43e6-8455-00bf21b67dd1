e0acfcf59cb45ee4fb8d5843e299557ae866d1b3
ch307 <bugatachandrasekhar09@gmail.com>
2023-07-17 09:24:03 +0000

    delete

--- springapp/src/main/java/com/examly/springapp/repository/LeadRepository.java
+++
@@ -1,8 +0,0 @@
-package com.examly.springapp.repository;
-import com.examly.springapp.model.Lead;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LeadRepository extends JpaRepository<Lead, Long> {
-}
