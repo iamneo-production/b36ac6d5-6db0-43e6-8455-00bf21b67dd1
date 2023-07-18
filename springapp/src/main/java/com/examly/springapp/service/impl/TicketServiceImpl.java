1ef396cf764b951626df83b960e52c69c69dbaa3
Manideep-Chembeti <manideep.chembeti0106@gmail.com>
2023-07-18 06:48:51 +0000

    Ticket

---
+++ springapp/src/main/java/com/examly/springapp/service/impl/TicketServiceImpl.java
@@ -0,0 +1,57 @@
+package com.examly.springapp.service.impl;
+
+import com.examly.springapp.Exception.ResourceNotFoundException;
+import com.examly.springapp.model.Ticket;
+import com.examly.springapp.repository.TicketRepository;
+import com.examly.springapp.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TicketServiceImpl implements TicketService {
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @Override
+    public List<Ticket> getAllTickets() {
+        return ticketRepository.findAll();
+    }
+
+    @Override
+    public Ticket getTicketById(Long ticketId) throws ResourceNotFoundException {
+        Optional<Ticket> ticket = ticketRepository.findById(ticketId);
+        if (ticket.isPresent()) {
+            return ticket.get();
+        } else {
+            throw new ResourceNotFoundException("Ticket not found with ID: " + ticketId);
+        }
+    }
+
+    @Override
+    public Ticket createTicket(Ticket ticket) {
+        return ticketRepository.save(ticket);
+    }
+
+    @Override
+    public Ticket updateTicket(Long ticketId, Ticket ticketDetails) throws ResourceNotFoundException {
+        Ticket ticket = getTicketById(ticketId);
+        ticket.setCustomer(ticketDetails.getCustomer());
+        ticket.setSubject(ticketDetails.getSubject());
+        ticket.setDescription(ticketDetails.getDescription());
+        ticket.setStatus(ticketDetails.getStatus());
+        ticket.setAssignedTo(ticketDetails.getAssignedTo());
+        ticket.setCreatedAt(ticketDetails.getCreatedAt());
+        ticket.setUpdatedAt(ticketDetails.getUpdatedAt());
+        return ticketRepository.save(ticket);
+    }
+
+    @Override
+    public void deleteTicket(Long ticketId) throws ResourceNotFoundException {
+        Ticket ticket = getTicketById(ticketId);
+        ticketRepository.delete(ticket);
+    }
+}
